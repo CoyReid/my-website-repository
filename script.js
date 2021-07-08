7a1491abfeb9c80f77a400c061e030d98e6d054d
Coy Reid <coyreid7@gmail.com>
2021-07-08 18:05:59 -0500

    added contact box/footer links

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.querySelector('body').appendChild(h2); */
 const welcome = document.getElementById("welcome-message");
 
 welcome.addEventListener("mouseover", function() {
-    welcome.style.backgroundColor = "purple";
+    welcome.style.backgroundColor = "indigo";
 })
 
 welcome.addEventListener("mouseout", function() {
